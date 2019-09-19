0ec56baa8a22707c2be4abac38c56f3a776c0c31
mrbreaks <52068352+mrbreaks@users.noreply.github.com>
2019-09-18 17:27:24 -0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 const pg = require("pg-promise")();
-const db = pg({process.env.DATABASE_URL,ssl: true,});
+const db = pg(process.env.DATABASE_URL);
+ ssl: true,
 module.exports = db;
